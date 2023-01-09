7a0ff6bb7fa1d5f90b97e7bd9273ce8b3b70bcec
LakhmatovSergei <svlakhmatov@gmail.com>
2023-01-09 17:33:56 +0300

    Initial commit

---
+++ src/main/java/ru/gb/lesson7_springdata_hw/Lesson7SpringDataHwApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.lesson7_springdata_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7SpringDataHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson7SpringDataHwApplication.class, args);
+    }
+
+}
